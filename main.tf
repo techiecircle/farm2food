b4c616292fef0d6509b48bb024d9cf3549130da5
Pradeep <6887172+techiecircle@users.noreply.github.com>
2022-06-27 16:46:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     google = {
-    source = "hashicorp/google"
-    version = "3.5.0"
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
   }
-}
 }
 
 provider "google" {
-  credentials = file("credentials.json")
+  credentials = file ("credentials.json")
   
   project = "<PROJECT_ID>"
   region = "us-central1"
